fabb68f2e5065aa46a5d5ecc889add93a4388427
vincent <vincentanthony.perez@gmail.com>
2025-02-24 21:53:42 +0800

    Day 16 Bug Fixing 1 (Python Mega Course)

---
+++ gui.py
@@ -0,0 +1,15 @@
+import functions
+import FreeSimpleGUI as sg
+
+label = sg.Text('Type in a to-do')
+input_box = sg.InputText(tooltip = 'Enter todo')
+add_button = sg.Button('Add')
+
+window = sg.Window('My To-Do App',
+                   layout = [[label],
+                             [input_box, add_button]],
+                   font = ('Helvetica',20))
+
+window.Read()
+print('Hello')
+window.Close()
